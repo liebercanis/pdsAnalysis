60a6b9650ca3f6069ef4fa3be8d70e38b86534d7
Yujing Sun <31490342+ysun7@users.noreply.github.com>
2017-11-15 18:12:42 -1000

    Add files via upload

--- macro/TOF_nspectrum.C
+++ macro/TOF_nspectrum.C
@@ -10,16 +10,17 @@
 
 //Loop through low intensity runs to get TOF and Neutron spectrum
 //require pmtAna_07-31-????_0.root
-int low_intensity_start=1518;//1518 low intensity starts
-int low_intensity_end=2134;//2130 low intensity ends
+int low_intensity_start=1600;//1518 low intensity starts
+int low_intensity_end=1600;//2130 low intensity ends
 double L=23.2;//m
 double c=0.299792458;//m/ns
 double nmass=939.565;//MeV
 double En;
 int sample_size=2100;
-double gain[21],gaine[21];
+double gain[21],gaine[21],rgain[21];
 int NPMT=21;
 double bins[481];
+double promptt_rft;
 
 Int_t readGainConstants()
 {
@@ -34,24 +35,25 @@ Int_t readGainConstants()
   gtree->GetEntry(0);
   printf(" \n \t using gains from file %s\n",filename.Data()); 
   goodGains->print();
-  for(int ipmt=1; ipmt<NPMT; ++ipmt) gain[ipmt]=goodGains->gain[ipmt]/goodGains->gain[0];
-  gain[0]=1.0;
+  for(int ipmt=0; ipmt<NPMT; ++ipmt) {gain[ipmt]=goodGains->gain[ipmt];}
+  for(int ipmt=1; ipmt<NPMT; ++ipmt) {rgain[ipmt]=goodGains->gain[ipmt]/goodGains->gain[0];}
+  rgain[0]=1.0;
   printf(" using normalized gains \n");
   for(int ipmt=0; ipmt<NPMT; ++ipmt) printf(" %i  %f ; ",ipmt,gain[ipmt]); 
   printf(" \n");
-
+  //for(int ipmt=1; ipmt<NPMT; ++ipmt) cout<<gain[ipmt]<<endl;
   return 21;
 }
 
 void TOF_nspectrum()
 {
-  // variable binning for n-spectrum
+  
   for (int tick=500;tick>=20;tick--)
   {
   bins[500-tick]=nmass*(sqrt(1/((tick*4*c/L)*(tick*4*c/L)-1)+1)-1);
   //cout<<bins[500-tick]<<endl;
   }
-  //return;
+  
   if(readGainConstants()==0) {
     printf(" cannot read gain constants file so abort \n");
     return;
@@ -66,7 +68,7 @@ void TOF_nspectrum()
   float trig,tmax,qmax,tmaxUn,qmaxUn,sumUn,noise,base,nhit;
   vector <double> run_number, trign[9];
   
-  TFile *outputfile = new TFile(Form("TOF_nspectrum_%d_%d.root",low_intensity_start,low_intensity_end),"recreate");
+  TFile *outputfile = new TFile(Form("TOF_nspectrum_pulse_%d_%d.root",low_intensity_start,low_intensity_end),"recreate");
   
     struct dirent *entry;
     DIR *dp;
@@ -79,13 +81,27 @@ void TOF_nspectrum()
      TH1F *promptt_rft_h = new TH1F("promptt_rft_h","Prompt Time - RFtime;Time (ns);Number of entries",2*sample_size,-4*sample_size,4*sample_size); 
      TH1F *tof_h = new TH1F("tof_h","TOF (Assuming L = 23.2 m);Time (ns);Number of entries",2*sample_size,-4*sample_size,4*sample_size); 
      TH1F *nspectrum_h = new TH1F("neutron_spectrum_h",";Neutron E_{Kin} (MeV);Frac of triggers",480,bins);
+     
+     TH2F *height_integral_ADC_h = new TH2F("height_integral_ADC_h",";PMT Pulse Height (ADC);PMT Pulse Integral (ADC)",500,0,10000,1500,0,30000);
+     TH2F *height_integral_PE_h = new TH2F("height_integral_PE_h",";PMT Pulse Height (PE);PMT Pulse Integral (ADC)",500,0,1000,1500,0,30000);
+     
+     TH2F *lightyield_nenergy_ADC_h = new TH2F("lightyield_nenergy_ADC_h","Light Yield VS n-Energy;n Energy (MeV);Light Yield (ADC)",480,bins,500,0,10000);
+     TH2F *lightyield_nenergy_PE_h = new TH2F("lightyield_nenergy_PE_h","Light Yield VS n-Energy;n Energy (MeV);Light Yield (PE)",480,bins,500,0,1000);
+     
+     TH1F *prompt_height_total_ratio_ADC_h = new TH1F("prompt_height_total_ratio_ADC_h","Prompt/Total;Ratio;Number of entries",100,0,1); 
+     TH1F *prompt_height_total_ratio_PE_h = new TH1F("prompt_height_total_ratio_PE_h","Prompt/Total;Ratio;Number of entries",100,0,1);
+     TH1F *prompt_integral_total_ratio_h = new TH1F("prompt_integral_total_ratio_h","Prompt/Total;Ratio;Number of entries",100,0,1);
+
+     TH1F *light_timing_h = new TH1F("light_timing_h",";Time (ns);Number of entries",10000,-10000,30000);
      //TH1F *tof_h = new TH1F("tof","tof",4200,-2100,2100);
+     
     while ((entry = readdir(dp))) {
      string filename=entry->d_name;
      std::size_t found=std::string::npos;
      found=filename.find("pmtAna_07-31-");
      if (found!=std::string::npos) {
        string runn = filename.substr (13,4);
+       //cout<<runn<<endl;
        istringstream buffer(runn);
        int value;
        buffer >> value;
@@ -146,41 +162,124 @@ void TOF_nspectrum()
 	  std::vector<TPmtHit> hit;
 		  
 	  int counter=0;
+	  //aSize=100;
 	  for(unsigned ientry =0; ientry < aSize; ++ientry ) {
+	    
 	    pmtTree->GetEntry(ientry);
 	    totalqUn=0;
 
 	    hit.clear();
 	    hit = ev->hit;
+	    //cout<<"####################  RF pulses #####################"<<endl;
+	    //cout<<"ev->rft21: "<<ev->run<<endl;
+	    //cout<<"ev->rft22: "<<ev->event<<endl;
 	    ntPmt->GetEntry(ientry*21);
-	    TH1F *sum_h = new TH1F("sum","sum",4200,-2100,2100);
+
+	    //cout<<endl;
+	    TH1F *sum_h = new TH1F("sum","sum",4200,-2100,2100);// temperarily sum all the peaks and take the maximum as prompt time; eventually will use summed channel
 	    //TCanvas *myc = new TCanvas("myc","histograms",1);
-	    
+
+   
+	    //cout<<"event "<<ientry<<endl;
+	    double ly=0,lyPE=0;
 	    if(trig==5) {
+	      	    rft=min(ev->rft21[0], ev->rft22[0]);
+		    rft=min(rft, ev->rft23[0]); 
+		    //cout<<hit.size()<<endl;
 		for(int ihit =0; ihit < hit.size(); ++ihit) {
 		      TPmtHit* phit = &(ev->hit[ihit]);
-	      
-		      rft=min(ev->rft21[0], ev->rft22[0]);
-		      rft=min(rft, ev->rft23[0]);
-		      int shiftt;
 
+		      int shiftt;
 		      if(phit->ipmt<7)shiftt=ev->rft21[0]-rft;
 		      else if(phit->ipmt<14)shiftt=ev->rft22[0]-rft;
 		      else if(phit->ipmt<21)shiftt=ev->rft23[0]-rft;
 		      
+		      double pulse_height=0;
+		      double pulse_integral=0;
 		      for (int i=0;i<phit->nsamples;i++) {
-		      sum_h->Fill(phit->tsample[i]-shiftt,phit->qsample[i]*gain[phit->ipmt]);// sum all the hits and take the maximum as prompt time; Time are shifted according to RF signal
+		      sum_h->Fill(phit->tsample[i]-shiftt,phit->qsample[i]*rgain[phit->ipmt]);// sum all the hits and take the maximum as prompt time; Time are shifted according to RF signal
+		      //cout<<phit->tsample[i]<<"	"<<shiftt<<"	"<<phit->qsample[i]*rgain[phit->ipmt]<<endl;
+		      pulse_height=max(pulse_height,phit->qsample[i]*rgain[phit->ipmt]);
+		      pulse_integral+=phit->qsample[i]*rgain[phit->ipmt];
 		      }
+		      if(pulse_height-phit->qUnpeak>0.1)
+		      {
+			cout<<"wrong !!!"<<endl;
+			cout<<pulse_height<<" 	"<<phit->qUnpeak<<endl;
+		      }
+		      /*if(pulse_integral/pulse_height>100) 
+		      {
+			cout<<"check event "<<ientry <<" pmt "<<phit->ipmt <<" time from "<<phit->tsample[phit->nsamples-1]<<" to "<<phit->tsample[0]<<endl;
+			cout<<pulse_height<<" 	"<<pulse_integral<<endl;
+		      }*/
+		      ly+=pulse_height;
+		      lyPE+=pulse_height/float (gain[phit->ipmt]);
+		      
+		      height_integral_ADC_h->Fill(pulse_height,pulse_integral);
+		      height_integral_PE_h->Fill(pulse_height/float (gain[phit->ipmt]),pulse_integral);
 		      ntHit->GetEntry(counter);
 		      totalqUn+=qUnhit;
 		      counter++;
 		      
 		    }
+
+
+  
 		    //sum_h->Draw();
 		    //myc->Update();sleep(2);
-		if(totalqUn>threshold)promptt_rft_h->Fill((sum_h->GetMaximumBin()-rft-2100)*4);
+		    //cout<<"haha"<<endl;
+		promptt_rft=sum_h->GetMaximumBin()-rft-2100;
+		//cout<<ientry<<" tprompt = "<<promptt_rft*4<<endl;
+		if(totalqUn>threshold){
+		lightyield_nenergy_ADC_h->Fill(nmass*(sqrt(1/(((promptt_rft*4+625)*c/L)*((promptt_rft*4+625)*c/L)-1)+1)-1),ly);  
+		lightyield_nenergy_PE_h->Fill(nmass*(sqrt(1/(((promptt_rft*4+625)*c/L)*((promptt_rft*4+625)*c/L)-1)+1)-1),lyPE);
+		promptt_rft_h->Fill(promptt_rft*4);
+		cout<<ientry<<" tprompt ... = "<<promptt_rft*4<<endl;
+		}
+		double promptpulse_height=0;
+		double promptpulse_height_PE=0;
+		double promptpulse_integral=0;
+		double delayedpulse_height=0;
+		double delayedpulse_integral=0;
+		for(int ihit =0; ihit < hit.size(); ++ihit) {
+		      TPmtHit* phit = &(ev->hit[ihit]);
+
+		      int shiftt;
+		      if(phit->ipmt<7)shiftt=ev->rft21[0]-rft;
+		      else if(phit->ipmt<14)shiftt=ev->rft22[0]-rft;
+		      else if(phit->ipmt<21)shiftt=ev->rft23[0]-rft;
+		      
+		      double temppulse_height=0;
+		      double temppulse_integral=0;
+		      
+		      for (int i=0;i<phit->nsamples;i++) {
+			//sum_h->Fill(phit->tsample[i]-shiftt,phit->qsample[i]*rgain[phit->ipmt]);
+			temppulse_height=max(temppulse_height,phit->qsample[i]*rgain[phit->ipmt]);
+			temppulse_integral+=phit->qsample[i]*rgain[phit->ipmt];
+		      }
+		      //cout<<"*** "<<phit->tsample[0]-shiftt-rft<<"--"<<phit->tsample[phit->nsamples-1]-shiftt-rft<<" < "<<promptt_rft+2<<endl;
+		      if( (phit->peakTime-shiftt-rft) <= promptt_rft+5 )//Unit tick(=4ns)
+		      {
+			promptpulse_height+=temppulse_height;
+			promptpulse_height_PE+=temppulse_height/float (gain[phit->ipmt]);
+			promptpulse_integral+=temppulse_integral;
+		      }
+		      else
+		      {
+			delayedpulse_height+=temppulse_height;
+			delayedpulse_integral+=temppulse_integral;
+		      }
+	      light_timing_h->Fill((phit->peakTime-shiftt-sum_h->GetMaximumBin()+2100)*4);
+		  }
+		  //cout<<ev->event<<"	"<<promptt_rft<<"	"<<rft<<"	"<<promptpulse_height<<"	"<<delayedpulse_height<<"	"<<ly<<endl;
+		if(delayedpulse_height>0)
+		{
+		  prompt_height_total_ratio_ADC_h->Fill(promptpulse_height/ly);
+		  prompt_height_total_ratio_PE_h->Fill(promptpulse_height_PE/lyPE);
+		}
+		if(delayedpulse_integral>0)prompt_integral_total_ratio_h->Fill(promptpulse_integral/(promptpulse_integral+delayedpulse_integral));
 		//if(totalqUn>threshold && (sum_h->GetMaximumBin()-rft-2100)*4<-800)cout<<ientry<<endl;
-	      }
+	      }      
 	    else {
 		counter+=hit.size();
 	      }
@@ -189,6 +288,7 @@ void TOF_nspectrum()
         }
       }
     }//end of while loop
+    
    
   TF1 *g1 = new TF1("g1","gaus",-640,-616);
   g1->SetLineColor(2);
@@ -201,16 +301,10 @@ void TOF_nspectrum()
     if(i>1945)tof_h->SetBinContent(i-g1->GetParameter(1)/4.+L/c/4.,promptt_rft_h->GetBinContent(i));
   }
 
-  /*for (int i=0;i<1000000;i++)
-  {
-     double t=tof_h->GetRandom(); 
-     En=nmass*(sqrt(1/((t*c/L)*(t*c/L)-1)+1)-1);
-     nspectrum_h->Fill(En);
-  }*/
-  for (int tick=2599;tick>=2119;tick--)//fill nspectrum_h from the slowest to the fastest neutrons.
+  for (int tick=2599;tick>=2119;tick--)
   {
      double height=tof_h->GetBinContent(tick); 
-     cout<<height<<endl;
+     //cout<<height<<endl;
      //En=nmass*(sqrt(1/((t*c/L)*(t*c/L)-1)+1)-1);
      nspectrum_h->SetBinContent(2600-tick,height);
   }
@@ -220,7 +314,17 @@ void TOF_nspectrum()
   promptt_rft_h->Write();
   tof_h->Write();
   nspectrum_h->Write();
+  height_integral_ADC_h->Write();
+  height_integral_PE_h->Write();
+  prompt_height_total_ratio_ADC_h->Write();
+  prompt_height_total_ratio_PE_h->Write();
+  prompt_integral_total_ratio_h->Write();
+  lightyield_nenergy_ADC_h->Write();
+  lightyield_nenergy_PE_h->Write();
+  light_timing_h->Write();
+  
   outputfile->Close();
+  
   delete outputfile;
 }
 
